013c803f5a40f376ec284719b9de35df459ec39c
James Mathis <mathis_james@outlook.com>
2018-08-21 18:46:35 -0400

    updated unit test script

--- unittest.sh
+++ unittest.sh
@@ -1,2 +1,44 @@
-python -B -m unit.test_mymath -v
-#python -B -m unittest discover -v
+#!/bin/bash
+#-----------------------------------------------------------------------------#
+# A P P  I N F O R M A T I O N #
+#-----------------------------------------------------------------------------#
+#
+# NAME: UNITTEST FOR PYTHON / JENKINS PROJECTS
+# DATE: 8/21/2018
+# CREATOR: James Mathis
+#
+# DESCRIPTION:
+#
+# VERSION: 1.0
+#
+#-----------------------------------------------------------------------------#
+
+#-----------#
+# variables #
+#-----------#
+
+UNITTEST_MODULE='unit.test_mymath'
+
+#-----------#
+# FUNCTIONS #
+#--------------------------------------#
+
+# check if user is ROOT #
+function empty() {
+
+
+}
+
+#--#
+
+
+#----------#
+# MAINLOOP #
+#-------------------------------------#
+
+# start uniit testing #
+python -B -m $UNITTEST_MODULE -v
+
+
+#--#
+
